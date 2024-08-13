086659b4dc092b8a07946f5e2aaff95796eaefa7
João Claudio de Andrade Bento <74563015+joaocla@users.noreply.github.com>
2024-08-13 17:53:18 -0300

    consome api e lista pokemons
    listando pokemons de 1 a 10 por enquanto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,28 @@
 
-console.log('Ola!');
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
+function createPokemonsLi(pokemon){
+    return `<li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="details">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">
+                </div>
+            </li>`
+};
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons) => {
+    for(i = 0; i < pokemons.length; i++){
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += createPokemonsLi(pokemon)
+        }
+})
